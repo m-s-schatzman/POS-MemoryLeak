8ddd684f52b568780e8eec62318e7ebc598931a8
cameronclifton <camclifton@gmail.com>
2016-02-21 14:42:11 -0500

    Update Inventory.java

--- Application/Model/Inventory.java
+++ Application/Model/Inventory.java
@@ -3,13 +3,15 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package makefiletest;
+
 
 /**
  *
  * @author Cameron
  */
+ import java.util.ArrayList;
 public class Inventory {
-public  ArrayList<Item> items = new ArrayList<Item>( );
+
+ ArrayList<Item> items;
     
 }
