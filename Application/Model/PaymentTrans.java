468acec559832d2d0db4882c81c9cb1706f60350
jjy216 <jjy216@lehigh.edu>
2016-02-21 14:37:03 -0500

    Added files via upload

---
+++ Application/Model/PaymentTrans.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+*/
+
+import java.util.Scanner;
+     
+ /*
+ * @author Jason
+ */
+public class PaymentTrans {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static String PaymentAuth (long cardID, int cash, int TotalPay) {
+        Scanner ms = new Scanner(System.in);
+        System.out.println("Payment Option: Cash or Card?");
+        String option = ms.next();
+        if (option == "Cash") {
+            if (cash == TotalPay) {
+                return "Success! Payment has been authorized";
+            }
+            else {
+                return "Payment fail! Please check amount";
+            }
+        }
+        else if (option == "Card") {
+            if (cardID == TotalPay) {
+                return "Success! Payment has been authorized";
+            }
+            else {
+                return "Payment fail! Please check card or amount";
+            }
+        }
+    }
+}
