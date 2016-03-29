f906658cc8a15a3d20091f155ac440177def73c5
Adam Svetec <adamsvetec@gmail.com>
2016-03-29 19:36:04 -0400

    Replace ManageUserController that is created with login frame. All Controllers should now be accessible through POSView

--- Application/Main.java
+++ Application/Main.java
@@ -4,8 +4,8 @@ public class Main{
   public static void main(String[] arg){
   	 JFrame loginFrame = new JFrame("Login");
   	 LoginController login = new LoginController(loginFrame);
-  	 JFrame ManageUFrame=new JFrame("MANAGE USER");
-  	  ManageUserController MUC= new ManageUserController(ManageUFrame);
-  	  
-  	    }
+  	 //Can comment following two lines in and comment out two above to skip login stage
+  	 //JFrame posFrame = new JFrame("Memory Leak POS");
+  	 //POSController posController = new POSController(posFrame);
+  	}
 }
