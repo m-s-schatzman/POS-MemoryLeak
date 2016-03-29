abc6f404426c37397c36629383810841c564a267
Adam Svetec <adamsvetec@gmail.com>
2016-03-28 23:06:35 -0400

    Add persistent storage main method to create new local database stored in Output folder

--- Application/Model/PersistentStorage.java
+++ Application/Model/PersistentStorage.java
@@ -1,4 +1,11 @@
 //Class used to connect to the locally stored derby database
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class PersistentStorage {
 
 	private static PersistentStorage persistentStorage;
@@ -12,5 +19,28 @@ public class PersistentStorage {
 			persistentStorage = new PersistentStorage();
 		}		
 		return persistentStorage;
+	}
+
+	private Connection getConnection(){
+		try{
+			return DriverManager.getConnection("jdbc:derby:Output/POSDatabase");
+		}
+		catch(SQLException sqle){
+			System.out.println(sqle.getMessage());
+			return null;
+		}
+	}
+
+	//Calling the main method of this class will create a new database
+    //Should not be done if database already created
+	public static void main(String[] args){
+		Connection conn = null;
+		try{
+			conn = DriverManager.getConnection("jdbc:derby:Output/POSDatabase;create=true");
+			conn.close();
+		}
+		catch(SQLException sqle){
+			System.out.println(sqle.getMessage());
+		}
 	}
 }
