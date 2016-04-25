766f11d9dfa297da3766a1a591eb1bef92c98d2a
Adam Svetec <adamsvetec@gmail.com>
2016-04-25 16:30:27 -0400

    Add logging at system startup and shutdown

--- Application/Main.java
+++ Application/Main.java
@@ -4,13 +4,15 @@
 /*------------------------------------------------------------------------------*/
 
 import javax.swing.*;
+import java.util.Date;
 
 public class Main{
   public static void main(String[] arg){
   	 DBConnection.openConnection();
+     Logger.logError("Connection Opened Successfully : "+ new Date().toString());
   	 createClosingHook();
   	 new LoginController();
-  	 //Can comment following two lines in and comment out two above to skip login stage
+  	 //Can comment following two lines in and comment out above lines to skip login stage
   	 //JFrame posFrame = new JFrame("Memory Leak POS");
   	 //POSController posController = new POSController(posFrame);
   	}
@@ -20,6 +22,7 @@ public class Main{
 		Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
     		public void run() {
     			DBConnection.closeConnection();
+          Logger.logError("Connection Closed Successfully : "+ new Date().toString());
     		}
 		}));
 	}
